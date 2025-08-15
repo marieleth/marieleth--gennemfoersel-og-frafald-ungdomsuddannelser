b07b3bef5e8346b6feca732d8303728384583a82
Marie Leth Hansen <marielhansen94@gmail.com>
2025-08-15 21:07:20 +0200

    Kode til analyse 1c, herunder udregning af landsgennemsnit på frafaldsprocent, Odenses placering ift. resterende kommuner og top-10 og bund-10 kommuner samt forberedelse af data til Power BI

--- R-script, analyse af gennemførsel og frafald.R
+++ R-script, analyse af gennemførsel og frafald.R
@@ -78,7 +78,7 @@ print("Fil gemt som: Power BI, analyse 1.csv")
 ##KLARGØRING AF DATA TIL POWER BI##
 #Opretter nyt data-frame til Danmarkskort-analyse
 kort_data <- data.frame(
-  Kommune = Analyse_1_datasæt$`Hele landet`, # Kopierer Kommune-data over i en ny variabel
+  Hele.landet = Analyse_1_datasæt$`Hele landet`, # Kopierer Kommune-data over i en ny variabel
   Frafaldsprocent = Analyse_1_datasæt$afbrudt_procent  # Kopierer frafalds-data over i ny variabel
 )
 
@@ -86,7 +86,7 @@ kort_data <- data.frame(
 kort_data <- kort_data[!is.na(kort_data$Frafaldsprocent), ]
 
 #Ret kommunenavnet Høje Taastrup så stavemåden fra Danmarks Statistik matcher stavemåden fra Json-filen i Power BI
-kort_data$Kommune <- gsub("-Taastrup", " Taastrup", kort_data$Kommune, useBytes = TRUE)
+kort_data$Hele.landet <- gsub("-Taastrup", " Taastrup", kort_data$Hele.landet, useBytes = TRUE)
 
 #Sørger for at Frafaldsprocent er numerisk og formateret korrekt
 kort_data$Frafaldsprocent <- as.numeric(kort_data$Frafaldsprocent)
@@ -102,4 +102,47 @@ print(kort_data)
 write.csv2(kort_data, "Power BI, analyse 1b Danmarkskort.csv", row.names = FALSE)
 print("Fil gemt som: Power BI, analyse 1b Danmarkskort.csv")
 
+##-----------------------------------------------------------------------------------------##
+###ANALYSE 1C: Landsgennemsnit på frafaldsprocent, Odenses placering ift. resterende kommuner og top-10 og bund-10 kommuner 
 
+
+#Del 1:ANALYSE
+#Beregner landsgennemsnittet af frafaldsprocenten, NA-værdier ignoreres
+gennemsnit_frafald <- mean(Analyse_1_datasæt$afbrudt_procent, na.rm = TRUE)
+
+#Konverter til procent
+gennemsnit_frafald_pct <- gennemsnit_frafald * 100
+print(paste("Gennemsnitlig frafaldsprocent i Danmark:", round(gennemsnit_frafald_pct, 2), "%"))
+
+#Find Odenses frafaldsprocent
+odense_frafald <- Analyse_1_datasæt$afbrudt_procent[odense_række]
+odense_frafald_pct <- odense_frafald * 100
+print(paste("Odenses frafaldsprocent:", round(odense_frafald_pct, 2), "%"))
+
+#Del 2:ANALYSE
+#Beregning Odenses placering (rang)
+#Sortér alle kommuner efter frafaldsprocent (højeste først)
+sorteret_frafald <- sort(Analyse_1_datasæt$afbrudt_procent, decreasing = TRUE, na.last = NA)
+odense_placering <- which(sorteret_frafald == odense_frafald)
+print(paste("Odenses placering:", odense_placering))
+
+#Del 3:ANALYSE + KLARGØRING AF DATA TIL POWER BI
+#Beregning af top-10 og bund-10 af kommuner ud fra frafaldsprocent
+top_10_højest <- Analyse_1_datasæt[order(Analyse_1_datasæt$afbrudt_procent, decreasing = TRUE), ][1:10, ]
+top_10_lavest <- Analyse_1_datasæt[order(Analyse_1_datasæt$afbrudt_procent, decreasing = FALSE), ][1:10, ]
+
+#Vis resultaterne
+print("Top 10 kommuner med højest frafaldsprocent:")
+print(top_10_højest[, c(3, 11)])
+
+print("Top 10 kommuner med lavest frafaldsprocent:")
+print(top_10_lavest[, c(3, 11)])
+
+
+# Gem top-10 og bund-10 til Power BI
+write.csv2(top_10_højest[, c(3, 11)], "Power BI, analyse 1c top-10 højest frafald.csv", row.names = FALSE)
+write.csv2(top_10_lavest[, c(3, 11)], "Power BI, analyse 1c bund-10 lavest frafald.csv", row.names = FALSE)
+
+print("Filer gemt som:")
+print("Power BI, analyse 1c top-10 højest frafald.csv")
+print("Power BI, analyse 1c bund-10 lavest frafald.csv")
